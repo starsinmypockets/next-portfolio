7f5d8f1ad5d5b69dda23f6c89c59664ecc68265e
Paul Walker <starsinmypockets@gmail.com>
2020-12-16 12:01:16 -0500

    Move menu to separate component

---
+++ components/menu.js
@@ -0,0 +1,65 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react"
+
+export default function Menu(props) {
+  const [showHamburger, setShowHamburger] = useState(false)
+  const hamburgerIsHidden = () =>
+    props.dimensions.width <= 744 && showHamburger == false
+
+  return (
+    <div id="menu">
+      <FontAwesomeIcon
+        className={
+          !hamburgerIsHidden()
+            ? "hidden"
+            : "" + "md:hidden fixed top-0 m-2 w-4 text-gray-200 shadow-lg"
+        }
+        icon={faBars}
+        onClick={() => setShowHamburger(true)}
+      />
+      <ul
+        className={
+          hamburgerIsHidden()
+            ? "hidden"
+            : "display-block" +
+              ` flex text-gray-300 text-2xl w-full md:w-auto bg-gray-600 bg-opacity-40 md:bg-transparent fixed top-1 right-0 md:mr-12 z-50`
+        }
+      >
+        <li>
+          <span
+            className="mr-6 ml-2 md:hidden text-gray-200"
+            onClick={() => setShowHamburger(false)}
+          >
+            X
+          </span>
+        </li>
+        <li>
+          <a href="#services" className="flex-1 mr-6">
+            Services
+          </a>
+        </li>
+        <li>
+          <a href="#work" className="flex-1 mr-6">
+            Work
+          </a>
+        </li>
+        <li>
+          <a href="#skills" className="flex-1 mr-6">
+            Skills
+          </a>
+        </li>
+        <li>
+          <a href="#about" className="flex-1 mr-6">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#contact" className="flex-1 mr-6">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import { useState, useEffect, useRef } from "react"
 import Head from "next/head"
 import styles from "../styles/Home.module.css"
-import { faBars } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Menu from "../components/menu.js"
 import About from "../components/about"
 import Skills from "../components/skills"
 import Services from "../components/services"
@@ -11,7 +11,6 @@ import Contact from "../components/contact"
 export default function Home() {
   const [menuOpen, setMenuOpen] = useState(false)
   const [dimensions, setDimensions] = useState({})
-  const [showHamburger, setShowHamburger] = useState(false)
 
   useEffect(() => {
     function handleResize() {
@@ -28,15 +27,7 @@ export default function Home() {
     }
   }, [])
 
-  console.log(
-    dimensions.width,
-    dimensions.height,
-    showHamburger,
-    dimensions.width < 764 && showHamburger == false
-  )
-
-  const hamburgerIsHidden = () =>
-    dimensions.width <= 766 && showHamburger == false
+  console.log(dimensions.width, dimensions.height)
 
   return (
     <div>
@@ -44,57 +35,7 @@ export default function Home() {
         <title>Paul Walker -- Professional Web Development</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <FontAwesomeIcon
-        className={
-          !hamburgerIsHidden()
-            ? "hidden"
-            : "" + "md:hidden fixed top-0 m-2 w-4 text-gray-200 shadow-lg"
-        }
-        icon={faBars}
-        onClick={() => setShowHamburger(true)}
-      />
-      <ul
-        className={
-          hamburgerIsHidden()
-            ? "hidden"
-            : "display-block" +
-              ` flex text-gray-300 text-2xl w-full md:w-auto bg-gray-600 bg-opacity-40 md:bg-transparent fixed top-1 right-0 md:mr-12 z-50`
-        }
-      >
-        <li>
-          <span
-            className="mr-6 ml-2 md:hidden text-gray-200"
-            onClick={() => setShowHamburger(false)}
-          >
-            X
-          </span>
-        </li>
-        <li>
-          <a href="#services" className="flex-1 mr-6">
-            Services
-          </a>
-        </li>
-        <li>
-          <a href="#work" className="flex-1 mr-6">
-            Work
-          </a>
-        </li>
-        <li>
-          <a href="#skills" className="flex-1 mr-6">
-            Skills
-          </a>
-        </li>
-        <li>
-          <a href="#about" className="flex-1 mr-6">
-            About
-          </a>
-        </li>
-        <li>
-          <a href="#contact" className="flex-1 mr-6">
-            Contact
-          </a>
-        </li>
-      </ul>
+      <Menu dimensions={dimensions} />
       <div id="home" className="h-screen w-full bg-rapeseed bg-top md:bg-cover">
         <div className="bg-blue-900 w-full h-60 md:h-40">
           <div className="text-center mt-6 md:mt-2 md:text-left absolute top-6">
@@ -123,8 +64,8 @@ export default function Home() {
       <Services />
       <div id="work" className="h-screen w-full">
         <h2 className="text-4xl p-6">Work</h2>
-          </div>
-        <About />
+      </div>
+      <About />
       <Skills />
       <Contact />
     </div>
