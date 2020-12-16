b7e7d49c7f38d113a2717843468a8acf8d6f55c8
Paul <starsinmypockets@gmail.com>
2020-12-16 22:18:24 +0000

    Export on build

--- components/contact.js
+++ components/contact.js
@@ -2,7 +2,11 @@ export default function Contact() {
   return (
     <div id="contact" className="p-8 justify-center items-center h-screen mt-24 bg-skyline bg-bottom bg-cover bg-opacity-20">
       <h2 className="text-4xl mb-4">Contact</h2>
-        <form>
+        <form onSubmit={(e) => {
+              console.log(e)
+              })
+            }}
+          >
           <div className="w-full">
             <input
               className="bg-gray-200 shadow-inner rounded-l p-2 w-full md:w-4/12"
@@ -23,6 +27,7 @@ export default function Contact() {
               placeholder="Type your message"
             />
           </div>
+          <input type="hidden" name="to" value="pjwalker76@gmail.com" />
           <button
             className="bg-blue-600 hover:bg-blue-700 duration-300 text-white shadow p-2 rounded-r"
             type="submit"
