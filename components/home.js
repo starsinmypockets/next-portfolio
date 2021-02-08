19671f70ddb4fd6997ecefc5c403609d6b4a3945
Paul <starsinmypockets@gmail.com>
2021-02-08 18:56:06 +0000

    Saturn home page header - 1

--- components/home.js
+++ components/home.js
@@ -1,14 +1,19 @@
 export default function Home() {
   return (
-    <div id="home" className="min-h-screen w-full bg-skyline bg-top md:bg-cover">
-      <div className="w-full h-60 md:h-40">
-        <div className="text-center mt-6 md:mt-2 md:text-left absolute top-6">
-          <h1 className="text-4xl font-black pl-6 pt-6 w-full">Paul Walker</h1>
-          <h2 className="text-3xl font-black mt-2 pl-6 w-full">
-            Professional Web Development
-          </h2>
+    <div id="home">
+      <div className="w-full h-60 md:h-40 bg-saturn-top relative">
+        <img src="/me_round_glasses_square.jpg"
+          alt="Paul Walker"
+          className="h-32 rounded-full absolute md:top-24 ml-4"
+        />
+        <div className="text-center mt-6 md:mt-2 md:text-left absolute bottom-2">
+          <h1 className="text-5xl text-green-500 font-black ml-40">Paul Walker</h1>
         </div>
       </div>
+        <h2 className="text-3xl text-black font-black mt-2 ml-40 w-full">
+          Professional Web Development
+        </h2>
+
         <div className="relative w-full md:w-4/12 mx-auto h-3/6 md:h-96 lg:h-72 bg-gray-400 bg-opacity-60 md:mt-32">
         <h4 className="text-2xl w-9/12 mx-auto pt-6 mb-6 text-center">
           Web Development & Design Services for the Modern Web.
@@ -22,6 +27,7 @@ export default function Home() {
           </button>
         </a>
       </div>
+      <div className="w-full h-30 md:h-40 bg-saturn-bottom"></div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
         "rapeseed": "url('/rape-field-in-spring-time-1633494.webp')",
         "skyline": "url('/philly_skyline_bg.webp')",
         "boathouse-row": "url('/boathouse_row_bg.webp')",
+        "saturn-top": "url('/saturn-rings-top.jpg')",
+        "saturn-bottom": "url('/saturn-rings-bottom.jpg')",
       }),
     },
   },
